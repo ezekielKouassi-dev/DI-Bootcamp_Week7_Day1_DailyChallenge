a28d33105172ae686fda5928c080e2e53590a215
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-02-12 17:05:36 +0000

    first commit

---
+++ src/main/java/di/atos/dailychallengeweek7day1/BaseClass.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package di.atos.dailychallengeweek7day1;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class BaseClass {
+
+    public void display() {
+        System.out.println("Méthode de la classe de base");
+    }
+}
---
+++ src/main/java/di/atos/dailychallengeweek7day1/DailyChallengeWeek7Day1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package di.atos.dailychallengeweek7day1;
+
+/**
+ *
+ * @author ezekielkouassi
+ */
+public class DailyChallengeWeek7Day1 {
+
+    public static void main(String[] args) {
+        DerivedClass derivedObject = new DerivedClass();
+
+        derivedObject.display();
+    }
+}
